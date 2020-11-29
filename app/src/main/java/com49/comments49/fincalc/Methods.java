ae7e15a17be427500b94c27fc5ba011fe0f56be7
Serrggor49 <56230469+Serrggor49@users.noreply.github.com>
2020-11-29 15:27:02 +0300

    Улучшил метод разделяющий число пробелом. Теперь нет проблемы, когда запятая также разделялась пробелом

--- app/src/main/java/com49/comments49/fincalc/Methods.java
+++ app/src/main/java/com49/comments49/fincalc/Methods.java
@@ -3,13 +3,28 @@ package com49.comments49.fincalc;
 public class Methods {
 
 
-    static String separate(String str){ // подаем на вход строку и добавляем в нее пробелы  для улучшения читабельности (было - 3000000, стало 3 000 000)
+    static String separate(String str) { // подаем на вход строку и добавляем в нее пробелы  для улучшения читабельности (было - 3000000, стало 3 000 000)
 
         StringBuilder s = new StringBuilder(str.replace(" ", ""));
-        for(int i = s.length(); i > 0; i = i-3){
-            s.insert(i, " ");
+
+
+        if (s.length() > 3) {
+            if (s.charAt(s.length() - 3) != ',') { // если полученное число имеет разделитель, то мы начинает отделять по 3 числа не с третьего, а с шестого символа, чтобы не было пробела перед запятой
+                System.out.println("888 " + s.charAt(s.length() - 3) + s.charAt(s.length() - 3));
+
+                for (int i = s.length(); i > 0; i = i - 3) {
+                    s.insert(i, " ");
+                }
+            } else {
+                System.out.println("777 " + s.charAt(s.length() - 5) + s.charAt(s.length() - 3));
+
+                for (int i = s.length() - 6; i > 0; i = i - 3) {
+                    s.insert(i, " ");
+                }
+            }
         }
-        return  s.toString().trim();
+        return s.toString().trim();
     }
+
 
 }
